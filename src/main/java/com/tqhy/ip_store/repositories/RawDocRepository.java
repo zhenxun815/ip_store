1b16a6d0073e5dce1a942b714da9bf1f652d547e
zhenxun815 <guoyiheng89@gmail.com>
2019-07-09 18:46:09 +0800

    add raw doc repository

---
+++ src/main/java/com/tqhy/ip_store/repositories/RawDocRepository.java
@@ -0,0 +1,39 @@
+package com.tqhy.ip_store.repositories;
+
+import com.tqhy.ip_store.models.mongo.RawDoc;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+
+/**
+ * @author Yiheng
+ * @create 7/9/2019
+ * @since 1.0.0
+ */
+public interface RawDocRepository extends MongoRepository<RawDoc, String> {
+
+    RawDoc findBy_id(ObjectId _id);
+
+    RawDoc findByPubId(String pubId);
+
+    List<RawDoc> findByAppId(String appId);
+
+    List<RawDoc> findBySection(String section);
+
+    List<RawDoc> findBySectionAndMainClass(String section, String mainClass);
+
+    List<RawDoc> findBySectionAndMainClassAndSubClass(String section, String mainClass, String subClass);
+
+    RawDoc save(RawDoc rawDoc);
+
+    List<RawDoc> save(List<RawDoc> rawDocs);
+
+    void deleteBy_id(ObjectId _id);
+
+    void deleteRawDocsByAppId(String appId);
+
+    void deleteRawDocByPubId(String pubId);
+
+}
