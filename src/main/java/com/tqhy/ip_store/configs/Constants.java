a51d508bd328b64bf3ba8f20c51512fd8517a326
zhenxun815 <guoyiheng89@gmail.com>
2019-07-09 18:45:05 +0800

    add constants

---
+++ src/main/java/com/tqhy/ip_store/configs/Constants.java
@@ -0,0 +1,15 @@
+package com.tqhy.ip_store.configs;
+
+/**
+ * @author Yiheng
+ * @create 2018/12/13
+ * @since 1.0.0
+ */
+public final class Constants {
+
+    public static final String XML_DATA_FORMAT_ORIGINAL = "original";
+
+    public static final String NEW_LINE = System.getProperty("line.separator");
+
+    public static final String ANSWER_SPLITOR = ",";
+}
