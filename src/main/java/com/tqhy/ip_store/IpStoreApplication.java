c66963d77ad1e508bce6b325e6ca683a0ca34dae
zhenxun815 <guoyiheng89@gmail.com>
2019-07-09 09:53:30 +0800

    first commit

---
+++ src/main/java/com/tqhy/ip_store/IpStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tqhy.ip_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tqhy/ip_store/IpStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tqhy.ip_store;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpStoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
