f46706c4c00c0bf55f50ba3f22cfb087ca4c154c
zhenxun815 <guoyiheng89@gmail.com>
2019-07-12 11:03:55 +0800

    1.add logger 2.modify api name and path

--- src/main/java/com/tqhy/ip_store/controllers/RawDocController.java
+++ src/main/java/com/tqhy/ip_store/controllers/RawDocController.java
@@ -2,18 +2,15 @@ package com.tqhy.ip_store.controllers;
 
 import com.tqhy.ip_store.models.mongo.RawDoc;
 import com.tqhy.ip_store.services.RawDocService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.lang.NonNull;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import sun.rmi.runtime.Log;
 
 /**
  * @author Yiheng
@@ -27,13 +24,25 @@ public class RawDocController {
     @Autowired
     RawDocService rawDocService;
 
-    @GetMapping("/{section}/{mainClass}/{subClass}")
+    Logger logger = LoggerFactory.getLogger(RawDocController.class);
+
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public Page<RawDoc> getRawDocs(@PathVariable(name = "section", required = false) String section,
-                                   @PathVariable(name = "mainClass", required = false) String mainClass,
-                                   @PathVariable(name = "subClass", required = false) String subClass,
-                                   @RequestParam(name = "pageNum", defaultValue = "0") int pageNum,
-                                   @RequestParam(name = "pageSize", defaultValue = "300") int pageSize) {
+    public Page<RawDoc> getAll(@RequestParam(name = "pageNum", defaultValue = "0") int pageNum,
+                               @RequestParam(name = "pageSize", defaultValue = "20") int pageSize) {
+        logger.info("request get all with page {}, size {}", pageNum, pageSize);
+        return rawDocService.findAll(PageRequest.of(pageNum, pageSize));
+    }
+
+    @GetMapping({"/class/{section}", "/class/{section}/{mainClass}", "/class/{section}/{mainClass}/{subClass}"})
+    @ResponseStatus(HttpStatus.OK)
+    public Page<RawDoc> getByClassification(@PathVariable(name = "section", required = false) String section,
+                                            @PathVariable(name = "mainClass", required = false) String mainClass,
+                                            @PathVariable(name = "subClass", required = false) String subClass,
+                                            @RequestParam(name = "pageNum", defaultValue = "0") int pageNum,
+                                            @RequestParam(name = "pageSize", defaultValue = "20") int pageSize) {
+        logger.info("request get by classification with {}/{}/{}, page {}, size {}",
+                    section, mainClass, subClass, pageNum, pageSize);
         if (StringUtils.isEmpty(section)) {
             return rawDocService.findAll(PageRequest.of(pageNum, pageSize));
         }
@@ -43,13 +52,14 @@ public class RawDocController {
         if (StringUtils.isEmpty(subClass)) {
             return rawDocService.findBySectionAndMainClass(section, mainClass, PageRequest.of(pageNum, pageSize));
         }
-        return rawDocService.findBuySectionAndMainClassAndSubClass(section, mainClass, subClass, PageRequest.of(pageNum, pageSize));
+        return rawDocService.findBySectionAndMainClassAndSubClass(section, mainClass, subClass, PageRequest.of(pageNum, pageSize));
     }
 
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public RawDoc getById(@PathVariable(name = "id") String id) {
+        logger.info("request get by id with {}", id);
         if (id.toLowerCase().startsWith("cn")) {
             if (id.contains(".")) {
                 return rawDocService.findByAppId(id).orElse(null);
