b73472b7820d7ad363d2c698f6bb5ee66639774f
zhenxun815 <guoyiheng89@gmail.com>
2019-08-05 18:06:56 +0800

    update test

--- src/test/java/com/tqhy/ip_store/XmlTest.java
+++ src/test/java/com/tqhy/ip_store/XmlTest.java
@@ -1,17 +1,14 @@
 package com.tqhy.ip_store;
 
-import com.alibaba.fastjson.JSONObject;
-import com.tqhy.ip_store.models.mongo.RawDoc;
 import com.tqhy.ip_store.models.xml.biblio.Biblio;
 import com.tqhy.ip_store.utils.XmlUtils;
 import org.junit.Test;
-import org.owasp.html.*;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 
 /**
@@ -25,18 +22,22 @@ public class XmlTest {
 
     @Test
     public void testParseXml() {
-        String xmlDir = "F:\\ip_data\\xml\\BIBLIOGRAPHIC_INVENTION_PUBLICATION\\CN2016102721435B";
-        String xmlName = "CN2016102721435B.XML";
+        String xmlDir = "F:\\ip_data\\xml\\BIBLIOGRAPHIC_INVENTION_PUBLICATION\\CN2016103052592B";
+        String xmlName = "CN2016103052592B.XML";
         File biblioXmlFile = new File(xmlDir, xmlName);
         Biblio biblio = XmlUtils.unmarshal(biblioXmlFile, Biblio.class);
-        logger.info("biblio is {}", XmlUtils.parseNode(biblio.getPatentDocument().getAbs().getParagraph()));
+        //logger.info("biblio is {}", XmlUtils.parseNode(biblio.getPatentDocument().getBiblioData().getTitle()));
+        String title = biblio.getPatentDocument().getBiblioData().getTitle().toString();
+        logger.info("title is {}", title);
+
         //RawDoc rawDoc = XmlUtils.getRawDocFromXml(new File(xmlDir)).orElse(null);
+        //rawDoc.set_id(new ObjectId());
         //logger.info("raw doc is: {}", JSONObject.toJSONString(rawDoc));
     }
 
     @Test
     public void testHtmlSanitizer() {
-        String text = "一种均<![CDATA[开始，由 ]]>相水热法制备花状Cu<sub>2</sub>V<sub>2</sub>O<sub>7</sub>材料的方法及制备的Cu<sub>2</sub>V<sub>2</sub>O<sub>7</sub>材料";
+        String text = "从POF<Sub>3</Sub>或PF<Sub>5</Sub>制造LiPO<Sub>2</Sub>F<Sub>2</Sub>";
         PolicyFactory policyFactory = new HtmlPolicyBuilder().toFactory();
         String sanitize = policyFactory.sanitize(text);
         logger.info("text is {}", sanitize);
