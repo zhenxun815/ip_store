3b31d69ae33a8ad6d6c30ab2fa4eaf99797883aa
zhenxun815 <guoyiheng89@gmail.com>
2019-08-29 11:56:06 +0800

    add test methods

--- src/test/java/com/tqhy/ip_store/MongoTest.java
+++ src/test/java/com/tqhy/ip_store/MongoTest.java
@@ -1,9 +1,13 @@
 package com.tqhy.ip_store;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.tqhy.ip_store.configs.Constants;
 import com.tqhy.ip_store.models.mongo.RawDoc;
 import com.tqhy.ip_store.repositories.RawDocRepository;
 import com.tqhy.ip_store.services.RawDocService;
+import com.tqhy.ip_store.utils.FileUtils;
+import com.tqhy.ip_store.utils.StringUtils;
 import com.tqhy.ip_store.utils.XmlUtils;
 import org.bson.types.ObjectId;
 import org.junit.Test;
@@ -17,8 +21,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Yiheng
@@ -80,4 +90,89 @@ public class MongoTest {
         doc.set_id(new ObjectId());
         logger.info("doc to save is {}", JSONObject.toJSONString(doc));
     }
+
+    @Test
+    public void testCountClassification() {
+        Pattern compile = Pattern.compile("(?<section>[A-Z])(?<mainClass>[0-9]{2})(?<subClass>[A-Z])");
+
+        String clfFilePath = "F:\\ip_data\\ip_search\\classification\\clf.txt";
+        String clfCountFilePath = "F:\\ip_data\\ip_search\\classification\\clf_count.txt";
+        File clfFile = new File(clfFilePath);
+        File clfCountFile = new File(clfCountFilePath);
+        AtomicInteger lt25Count = new AtomicInteger(0);
+        AtomicInteger lt2500Count = new AtomicInteger(0);
+        if (FileUtils.createNewFile(clfCountFile)) {
+            FileUtils.readLine(clfFile, line -> {
+                Matcher matcher = compile.matcher(line);
+                if (matcher.matches()) {
+                    String section = matcher.group("section");
+                    String mainClass = matcher.group("mainClass");
+                    String subClass = matcher.group("subClass");
+                    long count = rawDocService.countBySectionAndMainClassAndSubClass(section, mainClass, subClass);
+                    if (count < 25) {
+                        lt25Count.incrementAndGet();
+                    }
+                    if (count < 2500) {
+                        lt2500Count.incrementAndGet();
+                    }
+
+                    logger.info("clf is {}, count is {}", line, count);
+                    FileUtils.appendFile(clfCountFile,
+                                         line,
+                                         builder -> builder.append(":").append(count).append(Constants.NEW_LINE),
+                                         true);
+                } else {
+                    logger.info("not match {}", line);
+                }
+            });
+        } else {
+            logger.info("create clf count file [{}] failed", clfCountFilePath);
+        }
+
+        logger.info("lt25count is {},lt2500count is {}", lt25Count.get(), lt2500Count.get());
+    }
+
+    @Test
+    public void testWrite2Local() {
+        Pattern compile = Pattern.compile("(?<section>[A-Z])(?<mainClass>[0-9]{2})(?<subClass>[A-Z])");
+        String clfCountFilePath = "F:\\ip_data\\ip_search\\classification\\clf_count.txt";
+        String clfJsonDirPath = "E:\\ip_data\\clfs\\seged\\limit2500";
+        File clfCountFile = new File(clfCountFilePath);
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        FileUtils.readLine(clfCountFile, ":", split -> {
+            String clfStr = split[0];
+            int count = Integer.parseInt(split[1]);
+            if (count < 25) {
+                return;
+            }
+
+            Matcher matcher = compile.matcher(clfStr);
+            if (matcher.matches()) {
+                String section = matcher.group("section");
+                String mainClass = matcher.group("mainClass");
+                String subClass = matcher.group("subClass");
+                logger.info("clf is {}, count is {}", clfStr, count);
+
+                Page<RawDoc> rawDocPage =
+                        rawDocService.findBySectionAndMainClassAndSubClass(section,
+                                                                           mainClass,
+                                                                           subClass,
+                                                                           PageRequest.of(0, 2500));
+
+                pool.submit(() -> {
+                    List<RawDoc> rawDocs = rawDocPage.getContent();
+                    String clfJsonFileName = StringUtils.join(clfStr, "_", Integer.toString(rawDocs.size()), ".txt");
+                    File clfJsonFile = new File(clfJsonDirPath, clfJsonFileName);
+                    rawDocs.forEach(rawDoc -> FileUtils.appendFile(clfJsonFile,
+                                                                   JSON.toJSONString(rawDoc),
+                                                                   builder -> builder.append(Constants.NEW_LINE),
+                                                                   true));
+                });
+
+            } else {
+                logger.info("not match {}", clfStr);
+            }
+        });
+    }
+
 }
--- src/test/java/com/tqhy/ip_store/XmlTest.java
+++ src/test/java/com/tqhy/ip_store/XmlTest.java
@@ -1,7 +1,11 @@
 package com.tqhy.ip_store;
 
+import com.alibaba.fastjson.JSONObject;
+import com.tqhy.ip_store.models.mongo.RawDoc;
 import com.tqhy.ip_store.models.xml.biblio.Biblio;
+import com.tqhy.ip_store.utils.FileUtils;
 import com.tqhy.ip_store.utils.XmlUtils;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
@@ -9,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -22,17 +27,17 @@ public class XmlTest {
 
     @Test
     public void testParseXml() {
-        String xmlDir = "F:\\ip_data\\xml\\BIBLIOGRAPHIC_INVENTION_PUBLICATION\\CN2016103052592B";
-        String xmlName = "CN2016103052592B.XML";
+        String xmlDir = "F:\\ip_data\\xml\\BIBLIOGRAPHIC_INVENTION_PUBLICATION\\CN2011102246839A";
+        String xmlName = "CN2011102246839A.XML";
         File biblioXmlFile = new File(xmlDir, xmlName);
         Biblio biblio = XmlUtils.unmarshal(biblioXmlFile, Biblio.class);
         //logger.info("biblio is {}", XmlUtils.parseNode(biblio.getPatentDocument().getBiblioData().getTitle()));
         String title = biblio.getPatentDocument().getBiblioData().getTitle().toString();
         logger.info("title is {}", title);
 
-        //RawDoc rawDoc = XmlUtils.getRawDocFromXml(new File(xmlDir)).orElse(null);
-        //rawDoc.set_id(new ObjectId());
-        //logger.info("raw doc is: {}", JSONObject.toJSONString(rawDoc));
+        RawDoc rawDoc = XmlUtils.getRawDocFromXml(new File(xmlDir)).orElse(null);
+        rawDoc.set_id(new ObjectId());
+        logger.info("raw doc is: {}", JSONObject.toJSONString(rawDoc));
     }
 
     @Test
@@ -42,4 +47,5 @@ public class XmlTest {
         String sanitize = policyFactory.sanitize(text);
         logger.info("text is {}", sanitize);
     }
+
 }
